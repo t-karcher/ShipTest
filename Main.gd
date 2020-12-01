81e90430bcee20a981367ad90abf9fb187f0682e
Thomas Karcher <t_karcher@posteo.de>
2020-12-01 12:30:09 +0100

    ship subscene replaced with direct draw calls

--- Main.gd
+++ Main.gd
@@ -1,15 +1,27 @@
 extends Node2D
 
-var ShipScene = preload("res://Ship.tscn")
-onready var planet_pos = $Planet.position
-var ships = []
+const SHIP_COUNT = 5000
+
+onready var planet_pos : Vector2 = $Planet.position
+
+var ship_pos   : PoolVector2Array = []
+var ship_rot   : PoolRealArray    = []
+var ship_prox  : PoolRealArray    = []
+var ship_speed : PoolRealArray    = []
 
 func _ready():
-	for i in range (3000):
-		ships.append(ShipScene.instance())
-		add_child(ships[-1])
+	for _i in range (SHIP_COUNT):
+		ship_pos.append(planet_pos)
+		ship_rot.append(2 * PI * randf())
+		ship_prox.append(0.4 + randf() * 1.6)
+		ship_speed.append(140 + randf() * 20)
 
 func _process(delta):
-	for ship in ships:
-		ship.rotation = lerp_angle(ship.rotation, (planet_pos - ship.position).angle(), ship.prox * delta)
-		ship.position += Vector2(1,0).rotated(ship.rotation) * ship.speed * delta
+	for i in range (SHIP_COUNT):
+		ship_rot[i] = lerp_angle(ship_rot[i], (planet_pos - ship_pos[i]).angle(), ship_prox[i] * delta)
+		ship_pos[i] += Vector2(1,0).rotated(ship_rot[i]) * ship_speed[i] * delta
+	update()
+
+func _draw():
+	for i in range (SHIP_COUNT):
+		draw_circle(ship_pos[i], 3, Color.white)
--- Ship.gd
+++
@@ -1,4 +0,0 @@
-extends Polygon2D
-
-var prox : float = .4 + randf() * .2
-var speed : int = 140 + randf() * 20
